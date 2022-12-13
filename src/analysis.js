9864e0c9d30cd7daeb2cc9f9cefc70fc06b116ab
Onyinye-Reca <rebeccaobiajuru@gmail.com>
2022-12-13 21:37:33 +0100

    report and analysis

--- src/analysis.js
+++ src/analysis.js
@@ -1,4 +1,4 @@
-const { getTrips } = require('api');
+const { getTrips, getDriver } = require('api');
 
 /**
  * This function should return the trip data analysis
@@ -8,6 +8,95 @@ const { getTrips } = require('api');
  */
 async function analysis() {
   // Your code goes here
-}
+  
+  const responseData = await getTrips();
 
+  const numberConvertedData = responseData.map(object => {
+    object.billedAmount = parseFloat(String(object.billedAmount).replace(/,/g, ''));
+    return object;
+  })
+
+  const billed = {
+    noOfCashTrips: 0,
+    noOfNonCashTrips: 0,
+    billedTotal: 0,
+    cashBilledTotal: 0,
+    nonCashBilledTotal: 0
+  };
+
+  const singleDriverIdArray = [];
+  const tripCount = {};
+  const driversEarning = {};
+  for (let object of numberConvertedData) {
+    if (object.isCash === true) {
+      billed.noOfCashTrips = ++billed.noOfCashTrips;
+      billed.cashBilledTotal = billed.cashBilledTotal + object.billedAmount;
+    } else {
+      billed.noOfNonCashTrips = ++billed.noOfNonCashTrips;
+      billed.nonCashBilledTotal = billed.nonCashBilledTotal + object.billedAmount;
+    }
+
+    billed.billedTotal = billed.billedTotal + object.billedAmount;
+
+    if (!singleDriverIdArray.includes(object.driverID)) {
+      singleDriverIdArray.push(object.driverID);
+    }
+
+    if (object) {
+      tripCount[object.driverID] = ++tripCount[object.driverID] || 1;
+      driversEarning[object.driverID] = driversEarning[object.driverID] + object.billedAmount || object.billedAmount;
+    }
+  }
+  // billed
+
+  const tripCountValues = Object.values(tripCount);
+  const tripCountMax = Math.max(...tripCountValues);
+  const tripcountMaxIndex = tripCountValues.indexOf(tripCountMax);
+
+  const earningValues = Object.values(driversEarning);
+  const earningValuesMax = Math.max(...earningValues);
+  const earningValuesMaxIndex = earningValues.indexOf(earningValuesMax);
+
+  const driverPromise = [];
+  for (let driverId of singleDriverIdArray) {
+    driverPromise.push(getDriver(driverId));
+  }
+  
+  const driverPromiseStatus = await Promise.allSettled(driverPromise);
+  driverPromiseStatus
+  
+  let driverCounter = 0;
+  for (let driver of driverPromiseStatus) {
+    if (driver.status === 'fulfilled' && driver.value.vehicleID.length > 1) {
+      ++driverCounter;
+    }
+  }
+
+  const output = {};
+  output['noOfCashTrips'] = billed.noOfCashTrips;
+  output['noOfNonCashTrips'] = billed.noOfNonCashTrips;
+  output['billedTotal'] = Number(billed.billedTotal.toFixed(2));
+  output['cashBilledTotal'] = Number(billed.cashBilledTotal.toFixed(2));
+  output['nonCashBilledTotal'] = Number(billed.nonCashBilledTotal.toFixed(2));
+  output['noOfDriversWithMoreThanOneVehicle'] = driverCounter;
+  output['mostTripsByDriver'] = {
+    name: driverPromiseStatus[tripcountMaxIndex].value.name,
+    email: driverPromiseStatus[tripcountMaxIndex].value.email,
+    phone: driverPromiseStatus[tripcountMaxIndex].value.phone,
+    noOfTrips: tripCountMax,
+    totalAmountEarned: earningValues[tripcountMaxIndex]
+  };
+  output['highestEarningDriver'] = {
+    name: driverPromiseStatus[earningValuesMaxIndex].value.name,
+    email: driverPromiseStatus[earningValuesMaxIndex].value.email,
+    phone: driverPromiseStatus[earningValuesMaxIndex].value.phone,
+    noOfTrips: tripCountValues[earningValuesMaxIndex],
+    totalAmountEarned: earningValues[earningValuesMaxIndex]
+  };
+  output
+
+  return output;
+}
+analysis()
 module.exports = analysis;
+
--- src/report.js
+++ src/report.js
@@ -1,4 +1,4 @@
-const { getTrips } = require('api');
+const { getTrips, getDriver, getVehicle } = require('api');
 
 /**
  * This function should return the data for drivers in the specified format
@@ -9,6 +9,120 @@ const { getTrips } = require('api');
  */
 async function driverReport() {
   // Your code goes here
+  const response = await getTrips();
+
+  const driverIdArray = response.map(object => {
+    return object.driverID;
+  })
+
+  const singleDriverIdArray = Array.from(new Set(driverIdArray));
+
+  const output = [];
+  
+  const createUser = trip => {
+    const user = {
+      "user": trip.user.name,
+        "created": trip.created,
+        "pickup": trip.pickup.address,
+        "destination": trip.destination.address,
+        "billed": parseFloat(String(trip.billedAmount).replace(/,/g, '')),
+        "isCash": trip.isCash
+    };
+    return user;
+  }
+
+  const createVehicle = array => {
+    const vehicleArray = array.map(object => {
+      const plate = object.value.plate;
+      const manufacturer = object.value.manufacturer;
+      return {"plate": plate, "manufacturer": manufacturer};
+    })
+    return vehicleArray;
+  }
+
+  const driverPromise = [];
+  for (let driverId of singleDriverIdArray) {
+    driverPromise.push(getDriver(driverId));
+  }
+  
+  const driverPromiseStatus = await Promise.allSettled(driverPromise);
+
+  const resolvedDriverInfo = [];
+  for (let driver of driverPromiseStatus) {
+    if (driver.status === 'fulfilled') {
+      resolvedDriverInfo.push(driver);
+    }
+  }
+  console.log(resolvedDriverInfo);
+
+  const vehicleIdArray = resolvedDriverInfo.map(object => {
+    return object.value.vehicleID;
+  })
+  console.log(vehicleIdArray);
+
+  const vehiclePromise = [];
+  for (let array of vehicleIdArray) {
+    const promise = [];
+
+    for(let id of array) {
+      promise.push(getVehicle(id));
+    }
+    vehiclePromise.push(promise);
+  }
+  console.log(vehiclePromise);
+
+  const vehicleInfo = [];
+  for (let array of vehiclePromise) {
+    vehicleInfo.push(await Promise.allSettled(array))
+  }
+  console.log(vehicleInfo);
+
+  const vehicle = [];
+  for (let array of vehicleInfo) {
+    vehicle.push(createVehicle(array));
+  }
+
+  for (let i = 0; i < singleDriverIdArray.length; i++) {
+    const driverReport = {};
+    
+    if (driverPromiseStatus[i].status === 'fulfilled') {
+      driverReport['fullName'] = driverPromiseStatus[i].value.name;
+      driverReport['phone'] = driverPromiseStatus[i].value.phone;
+    }
+    
+    driverReport['id'] = singleDriverIdArray[i];
+    driverReport['vehicles'] = vehicle[i] || [];
+    driverReport['noOfTrips'] = 0;
+    driverReport['noOfCashTrips'] = 0;
+    driverReport['noOfNonCashTrips'] = 0;
+    driverReport['trips'] = [],
+    driverReport['totalAmountEarned'] = 0,
+    driverReport['totalCashAmount'] = 0,
+    driverReport['totalNonCashAmount'] = 0
+    for (let object of response) {
+      if (singleDriverIdArray[i] === object.driverID) {
+        driverReport.noOfTrips = ++driverReport.noOfTrips;
+        driverReport.trips.push(createUser(object));
+        const billedAmount = driverReport.totalAmountEarned + parseFloat(String(object.billedAmount).replace(/,/g, ''));
+        driverReport.totalAmountEarned = Number(billedAmount.toFixed(2));
+
+        if (object.isCash === true) {
+          driverReport.noOfCashTrips = ++driverReport.noOfCashTrips;
+          const billedAmount = driverReport.totalCashAmount + parseFloat(String(object.billedAmount).replace(/,/g, ''));
+          driverReport.totalCashAmount = Number(billedAmount.toFixed(2));
+        } else {
+          driverReport.noOfNonCashTrips = ++driverReport.noOfNonCashTrips;
+          const billedAmount = driverReport.totalNonCashAmount + parseFloat(String(object.billedAmount).replace(/,/g, ''));
+          driverReport.totalNonCashAmount = Number(billedAmount.toFixed(2));
+        }
+      }
+    }
+    output.push(driverReport);
+  }
+  
+  return output;
 }
+driverReport()
 
 module.exports = driverReport;
+
